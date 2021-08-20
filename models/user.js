71eb65ca33d3cf4af468feea47ab27400de12045
Victor <vjmreysantos@gmail.com>
2021-08-20 11:45:10 +0100

    user validation added

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import mongooseUniqueValidator from 'mongoose-unique-validator'
 
 const detailsSchema = new mongoose.Schema(
   {
@@ -27,6 +29,43 @@ const userSchema = new mongoose.Schema(
     isAdmin: { type: Boolean, default: false },
   }
 )
+
+userSchema.set('toJSON', 
+  {
+    transform(_doc, json) {
+      delete json.password
+      return json
+    },
+  }
+)
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function (passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation
+  })
+
+userSchema
+  .pre('validate', function(next) {
+    if (this.isModified('password') && this.password !== this.passwordConfirmation) {
+      this.invalidate('passwordConfirmation', 'does not match')
+    }
+    next()
+  })
+
+userSchema
+  .pre('save', function(next) {
+    if (this.isModified('password')) {
+      this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
+    }
+    next()
+  })
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+userSchema.plugin(mongooseUniqueValidator)
 
 const User = mongoose.model('User', userSchema)
 
