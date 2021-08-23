c819f3184708dd460fac8b5de39af0334b7c8f3d
Victor <vjmreysantos@gmail.com>
2021-08-23 10:05:58 +0100

    group data added to seed

--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,10 @@ import Event from '../models/event.js'
 import eventData from './data/events.js'
 import User from '../models/user.js'
 import userData from './data/users.js'
+import Group from '../models/group.js'
+import groupData from './data/groups.js'
+// import OnlineEvent from '../models/onlineEvent.js'
+// import onlineEventData from './data/onlineEvents.js'
 import { connectToDatabase, truncateDatabase, disconnectDatabase } from './helpers.js'
 
 async function seed() {
@@ -19,7 +23,13 @@ async function seed() {
     const event = await Event.create(eventData)
     console.log(`${event.length} Event added to the database`)
 
-    console.log('Goodby')
+    const group = await Group.create(groupData)
+    console.log(`${group.length} Group added to the database`)
+
+    // const onlineEvent = await OnlineEvent.create(onlineEventData)
+    // console.log(`${onlineEvent.length} Online Event added to the database`)
+
+    console.log('Goodbye')
   } catch (err) {
     console.log('something went wrong')
     console.log(err)
