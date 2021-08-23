27746c26eefbf1a0391ad14aa5c78dae596e5e28
DustySvensson <nessaswanson@hotmail.com>
2021-08-23 09:14:01 +0100

    users-seeding

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String },
+    password: { type: String, required: true },
     avatar: { type: String, required: true },
     house: { type: String, required: true },
     details: { detailsSchema },
