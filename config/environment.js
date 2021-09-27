a0a1bd925cd22b32bb994ca9ac4c776dda7096c0
Victor <vjmreysantos@gmail.com>
2021-09-27 16:00:57 +0100

    prep

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,11 @@
 // export const dbURI = 'mongodb://localhost/muggles'
-export const port = 4000
-export const secret = 'this is the secret'
+// export const port = 4000
+// export const secret = 'this is the secret'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI =
+  process.env.DB_URI || 'mongodb://localhost/muggles'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { port } from './config/environment.js'
 import { connectToDatabase } from './db/helpers.js'
 import router from './config/router.js'
@@ -8,7 +9,8 @@ import errorHandler from './lib/errorHandler.js'
 const app = express()
 
 app.use(express.json())
-app.use('/', logger)
+app.use(cors())
+app.use(logger)
 app.use('/api', router)
 app.use(errorHandler)
 
